599a1798dd396820d3de40531c166b30031373c0
Stevie Burnette <Diamond@Mitchells-MacBook-Pro.local>
2018-11-24 23:43:43 -0500

    added landing page and css need to fix animations

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,21 @@
 const app = angular.module('MyApp', []);
 
-app.controller('MainController', function(){
-  this.hello = 'oh hai!';
-});
+app.controller('MyController', ['$http', function($http){
+
+  this.includePath = 'partials/home.html';
+  this.navPath = 'partials/nav.html'
+
+  this.changeInclude = (path) => {
+    this.includePath = 'partials/'+ path +'.html';
+    console.log(this.changeInclude);
+  };
+
+  this.changeNavPath = (path) => {
+    this.navPath = 'partials/'+ path
++ '.html';
+console.log(this.changeNavPath);
+};
+
+    const controller = this;
+
+}]);
